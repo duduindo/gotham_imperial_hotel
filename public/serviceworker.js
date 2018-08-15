4e9c0a4ccc9c043663062edbfc6dee71597f3fac
Eduardo Paixão <duduindo@gmail.com>
2018-08-15 15:21:42 -0300

    Capter Creating HTML Responses - Location 517

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,22 +1,24 @@
 
-const cssCache = `
-	.hotel-slogan { 
-		background-color: black; 
-	} 
-
-	nav { 
-		display: none;
-	}
+const responseContent = `
+	<html>
+		<head>
+			<style>
+				body { text-align: center; background-color: #333; color: #eee; }
+			</style>
+		</head>
+		<body>
+			<h1>Gotham Imperial Hotel</h1>
+			<p>There seems to be a problem with your connection.</p>
+			<p>We look forward to telling you about our hotel as sons as you go online.</p>
+		</body>
+	</html>				
 `;
+
 
 self.addEventListener('fetch', event => {
 	event.respondWith(
 		fetch(event.request).catch(() => {
-			return new Response(`
-				Welcome to the Gotham Imperial Hotel.
-				There seems to be a problem with your connection.
-				We look forward to telling you about our hotel as sons as you go online
-			`)
+			return new Response(responseContent, { headers: {'Content-Type': 'text/html'} });
 		})
 	);
 });
