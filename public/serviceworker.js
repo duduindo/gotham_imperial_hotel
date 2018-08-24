4c8e879bc620f7c0045b5084c9eee81945b06895
Eduardo Paixão <duduindo@gmail.com>
2018-08-24 17:01:32 -0300

    Capter Retrieving Requests from CacheStorage - Location 712

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,25 +1,17 @@
 
-const responseContent = `
-	<html>
-		<head>
-			<style>
-				body { text-align: center; background-color: #333; color: #eee; }
-			</style>
-		</head>
-		<body>
-			<h1>Gotham Imperial Hotel</h1>
-			<p>There seems to be a problem with your connection.</p>
-			<p>We look forward to telling you about our hotel as sons as you go online.</p>
-		</body>
-	</html>				
-`;
+self.addEventListener('install', event => {
+	event.waitUntil(
+			caches.open('gith-cache').then(cache => {
+				return cache.add('/index-offline.html');
+			})
+	);
+});
 
 
 self.addEventListener('fetch', event => {
 	event.respondWith(
 		fetch(event.request).catch(() => {
-			return new Response(responseContent, { headers: {'Content-Type': 'text/html'} });
+			return caches.match('/index-offline.html');
 		})
 	);
 });
-
