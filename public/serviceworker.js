81fa4e013ac11d178c2fb43952bcd45e6830f010
Eduardo Paixão <duduindo@gmail.com>
2018-08-27 18:58:53 -0300

    Capter Caching in Our Sample App - Location 729

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,17 +1,21 @@
 
+const CACHE_NAME = 'gith-cache';
+const CACHE_URLS = [
+	'/index-offline.html',
+	'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+	'/css/gih-offline.css',
+	'/img/jumbo-background-sm.jpg',
+	'/img/logo-header.png',
+];
+
+
 self.addEventListener('install', event => {
 	event.waitUntil(
-			caches.open('gith-cache').then(cache => {
-				return cache.add('/index-offline.html');
-			})
-	);
-});
-
-
-self.addEventListener('fetch', event => {
-	event.respondWith(
-		fetch(event.request).catch(() => {
-			return caches.match('/index-offline.html');
+		caches.open(CACHE_NAME).then(cache => {
+			console.log(cache);
+			
+			return cache.addAll(CACHE_URLS);
 		})
 	);
 });
+
