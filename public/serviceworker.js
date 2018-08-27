370836200efc17aedd220a5290c9dc3922ba430d
Eduardo Paixão <duduindo@gmail.com>
2018-08-27 19:22:13 -0300

    Capter Matching the Right Response to each Request - Location 798

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -13,9 +13,31 @@ self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(cache => {
 			console.log(cache);
-			
+
 			return cache.addAll(CACHE_URLS);
 		})
 	);
 });
 
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		// Fetch
+		fetch(event.request).catch(() => {
+
+			// Cache
+			return caches.match(event.request).then(response => {
+				console.warn('Response:', response);
+				console.warn('Event: ', event.request.headers.get('accept').includes('text/html'));
+
+				if (response) {
+					return response;
+				} else if (event.request.headers.get('accept').includes('text/html')) {
+					return caches.match('/index-offline.html');
+				}
+			});
+
+
+		})
+	);
+});
