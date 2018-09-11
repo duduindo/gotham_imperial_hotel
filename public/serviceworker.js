cf98f193b96a05138b7a88eafca135a88873a964
Eduardo Paixão <duduindo@gmail.com>
2018-09-11 15:47:23 -0300

    Implementing Our Caching Strategy - With URL - Location 1717

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -35,18 +35,34 @@ self.addEventListener('install', event => {
 });
 
 
-self.addEventListener('fetch', event => {
-	event.respondWith(
-		fetch(event.request).catch(() => {
-			return caches.match(event.request).then(response => {				
-				if (response) {
-					return response;
-				} else if (event.request.headers.get('accept').includes('text/html')) {					
-					return caches.match('/index.html');
-				}
-			});
-		})
-	);
+self.addEventListener('fetch', event => {	
+	const requestURL = new URL(event.request.url);
+
+	if (requestURL.pathname === '/' || requestURL.pathname === '/index.html') {
+
+		event.respondWith(
+			caches.open(CACHE_NAME).then(cache => {
+				return cache.match('/index.html').then(cachedResponse => {
+					const fetchPromise = fetch('/index.html').then(networkResponse => {
+						cache.put('/index.html', networkResponse.clone());
+						
+						return networkResponse;
+					});
+
+					return cachedResponse || fetchPromise;
+				});
+			})
+		);
+
+	} else if (CACHED_URLS.includes(requestURL.href) || CACHED_URLS.includes(requestURL.pathname)) {
+		event.respondWith(
+			caches.open(CACHE_NAME).then(cache => {
+				return cache.match(event.request).then(response => {
+					return response || fetch(event.request);
+				});
+			})
+		);
+	}
 });
 
 
