8abf122796683ae2b674c2ea80263ada21badde4
Eduardo Paixão <duduindo@gmail.com>
2018-11-01 17:11:14 -0300

    File reservations.js refactored

--- public/js/reservations-store.js
+++ public/js/reservations-store.js
@@ -1,128 +1,146 @@
 var DB_VERSION = 2;
 var DB_NAME = "gih-reservations";
 
-var openDatabase = function() {
-  return new Promise(function(resolve, reject) {
+//
+// Open Database
+//
+const openDatabase = () =>
+  new Promise((resolve, reject) => {
+
     // Make sure IndexedDB is supported before attempting to use it
-    if (!self.indexedDB) {
-      reject("IndexedDB not supported");
+    if (!self.IndexedDB) {
+      reject('IndexedDB not supported');
     }
-    var request = self.indexedDB.open(DB_NAME, DB_VERSION);
-    request.onerror = function(event) {
-      reject("Database error: " + event.target.error);
-    };
 
-    request.onupgradeneeded = function(event) {
-      var db = event.target.result;
-      var upgradeTransaction = event.target.transaction;
-      var reservationsStore;
-      if (!db.objectStoreNames.contains("reservations")) {
-        reservationsStore = db.createObjectStore("reservations",
-          { keyPath: "id" }
-        );
+    const request = self.IndexedDB.open(DB_NAME, DB_VERSION);
+
+    // Error
+    //
+    request.onerror = event => reject(`Database error: ${event.target.error}`);
+
+    // Upgrade needed
+    //
+    request.onupgradeneeded = event => {
+      const db = event.target.result;
+      const upgradeTransaction = event.target.transaction;
+      let reservationsStore = null;
+
+      if (!db.objectStoreNames.contains('reservations')) {
+        reservationsStore = db.createObjectStore('reservations', { keyPath: 'id' });
       } else {
-        reservationsStore = upgradeTransaction.objectStore("reservations");
+        reservationsStore = upgradeTransaction.objectStore('reservations');
       }
 
-      if (!reservationsStore.indexNames.contains("idx_status")) {
-        reservationsStore.createIndex("idx_status", "status", { unique: false });
+      if (!reservationsStore.indexNames.contains('idx_status')) {
+        reservationsStore.createIndex('idx_status', 'status', { unique: false });
       }
     };
 
-    request.onsuccess = function(event) {
-      resolve(event.target.result);
-    };
+    // Success
+    //
+    request.onsuccess = event => resolve(event.target.result);
   });
-};
 
-var openObjectStore = function(db, storeName, transactionMode) {
-  return db
-    .transaction(storeName, transactionMode)
-    .objectStore(storeName);
-};
 
-var addToObjectStore = function(storeName, object) {
-  return new Promise(function(resolve, reject) {
-    openDatabase().then(function(db) {
-      openObjectStore(db, storeName, "readwrite")
-        .add(object).onsuccess = resolve;
-    }).catch(function(errorMessage) {
-      reject(errorMessage);
-    });
+//
+// Open Object Store
+//
+const openObjectStore = (db, storeName, transactionMode) => db.transaction(storeName, transactionMode).objectStore(storeName);
+
+
+//
+// Add to Object Store
+//
+const addToObjectStore = (storeName, object) =>
+  new Promise((resolve, reject) => {
+    openDatabase()
+      .then(db => {
+        openObjectStore(db, storeName, 'readwrite').add(object).onsuccess = resolve;
+      })
+      .catch(errorMessage => {
+        reject(errorMessage);
+      });
   });
-};
 
-var updateInObjectStore = function(storeName, id, object) {
-  return new Promise(function(resolve, reject) {
-    openDatabase().then(function(db) {
-      openObjectStore(db, storeName, "readwrite")
-        .openCursor().onsuccess = function(event) {
-          var cursor = event.target.result;
+
+// Update in Object Store
+//
+const updateInObjectStore = (storeName, id, object) =>
+  new Promise((resolve, reject) => {
+    openDatabase()
+      .then(db => {
+        openObjectStore(db, storeName, 'readwrite').openCursor().onsuccess = event => {
+          const cursor = event.target.result;
+
           if (!cursor) {
-            reject("Reservation not found in object store");
+            reject('Reservation not found in object store');
           }
+
           if (cursor.value.id === id) {
             cursor.update(object).onsuccess = resolve;
             return;
           }
+
           cursor.continue();
         };
-    }).catch(function(errorMessage) {
-      reject(errorMessage);
-    });
+      })
+      .catch(errorMessage => {
+        reject(errorMessage);
+      });
   });
-};
 
-var getReservations = function(indexName, indexValue) {
-  return new Promise(function(resolve) {
-    openDatabase().then(function(db) {
-      var objectStore = openObjectStore(db, "reservations");
-      var reservations = [];
-      var cursor;
-      if (indexName && indexValue) {
-        cursor = objectStore.index(indexName).openCursor(indexValue);
-      } else {
-        cursor = objectStore.openCursor();
-      }
-      cursor.onsuccess = function(event) {
-        var cursor = event.target.result;
-        if (cursor) {
-          reservations.push(cursor.value);
-          cursor.continue();
+
+const getReservations = (indexName, indexValue) =>
+  new Promise(resolve => {
+    openDatabase()
+      .then(db => {
+        const objectStore = openObjectStore(db, "reservations");
+        const reservations = [];
+        let cursor = null;
+
+        if (indexName && indexValue) {
+          cursor = objectStore.index(indexName).openCursor(indexValue);
         } else {
-          if (reservations.length > 0) {
-            resolve(reservations);
-          } else {
-            getReservationsFromServer().then(function(reservations) {
-              openDatabase().then(function(db) {
-                var objectStore = openObjectStore(db, "reservations", "readwrite");
-                for (var i = 0; i < reservations.length; i++) {
-                  objectStore.add(reservations[i]);
-                }
-                resolve(reservations);
-              });
-            });
-          }
+          cursor = objectStore.openCursor();
         }
-      };
-    }).catch(function() {
-      getReservationsFromServer().then(function(reservations) {
-        resolve(reservations);
-      });
-    });
-  });
-};
 
-var getReservationsFromServer = function() {
-  return new Promise(function(resolve) {
-    if (self.$) {
-      $.getJSON("/reservations.json", resolve);
-    } else if (self.fetch) {
-      fetch("/reservations.json").then(function(response) {
-        return response.json();
-      }).then(function(reservations) {
-        resolve(reservations);
+        cursor.onsuccess = event => {
+          const cursor = event.target.result;
+
+          if (cursor) {
+            reservations.push(cursor.value);
+            cursor.continue();
+          } else {
+            if (reservations.length) {
+              resolve(reservations);
+            } else {
+              getReservationsFromServer()
+                .then(reservations => {
+                  openDatabase().then(db => {
+                    const objectStore = openObjectStore(db, 'reservations', 'readwrite');
+
+                    reservations.forEach(reservation => objectStore.add(reservation));
+
+                    resolve(reservations);
+                  });
+                });
+            }
+          }
+        };
+      }).catch(() => {
+        getReservationsFromServer().then(function(reservations) {
+          resolve(reservations);
+        });
       });
-    }
   });
-};
+
+
+//
+// Get Reservations from Server
+//
+const getReservationsFromServer = () =>
+  new Promise(resolve => {
+    fetch("/reservations.json")
+      .then(response => response.json())
+      .then(reservations => resolve(reservations));
+  });
